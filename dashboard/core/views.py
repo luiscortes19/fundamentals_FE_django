a62a73c69249396ea3e4f22cf3cfae8cf375c0ca
luiscortes19 <luisc@dynabrackets.com>
2021-03-20 21:29:53 -0700

    added a few customers

--- dashboard/core/views.py
+++ dashboard/core/views.py
@@ -1,8 +1,37 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, forms, login
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
 from django.views import View
 
-class Index(View):
-    template = 'core/index.html'
+from .models import Employee
+
+
+class Index(LoginRequiredMixin, View):
+    template = "core/index.html"
+    login_url = reverse_lazy("login")
 
     def get(self, request):
-        return render(request,self.template)
+        employees = Employee.objects.all()
+        return render(request, self.template, {"employees": employees})
+
+
+class Login(View):
+    template = "core/login.html"
+
+    def get(self, request):
+        form = AuthenticationForm()
+        return render(request, self.template, {"form": form})
+
+    def post(self, request):
+        post = request.POST
+        form = AuthenticationForm(post)
+        username = post["username"]
+        password = post["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("index")
+        else:
+            return render(request, self.template, {"form": form})
